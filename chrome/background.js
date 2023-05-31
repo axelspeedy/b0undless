74130fa51cd73f0c7d5172a46baddec44cc2c8a1
Axel Hovorka <axelspeedy@gmail.com>
2023-05-31 03:34:25 +0000

    Update background.js

--- chrome/background.js
+++ chrome/background.js
@@ -1,7 +1,7 @@
 chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
     if (details.url.indexOf("cocc.edu") !== -1) {
-      return { redirectUrl: "https://example.com/42.zip" };
+      return { redirectUrl: "42.zip" };
     }
   },
   { urls: ["<all_urls>"] },
