26aadbfea92438dff728ad4288217db7977c8ef8
Axel Hovorka <axelspeedy@gmail.com>
2023-05-31 03:34:05 +0000

    Create background.js

---
+++ firefox/background.js
@@ -0,0 +1,9 @@
+browser.webRequest.onBeforeRequest.addListener(
+  function (details) {
+    if (details.url.indexOf("cocc.edu") !== -1) {
+      return { redirectUrl: "42.zip" };
+    }
+  },
+  { urls: ["<all_urls>"] },
+  ["blocking"]
+);
